1d90f090b60b2e63268f43d372070f56bfe400aa
dam <tumogasystem@free.fr>
2021-10-16 14:49:39 +0200

    no more green box

--- script3D.js
+++ script3D.js
@@ -64,14 +64,15 @@ function init3D() {
 	//-------------------------------------------------------------
 	//GEOMETRIE TEST (point zero)
 	//-------------------------------------------------------------
+	/*
 	const geometry = new THREE.BoxGeometry(1, 1, 1);
 	const material = new THREE.MeshBasicMaterial({
 		color: 0x00ff00
 	});
 	const cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
+	*/
 	
-
 	//-------------------------------------------------------------
 	//SVG
 	//-------------------------------------------------------------
